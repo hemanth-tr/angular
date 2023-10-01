45134ac83bc9e877db23ca43ce50181991b6053e
hemanth-tr <trhemanth2@gmail.com>
2023-10-01 21:51:16 +0530

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'my-app',
-	template: `<h1>Hello from {{ name }}!</h1>`,
+	templateUrl: './app.component.html',
 })
 export class AppComponent {
 	name = 'Angular';
